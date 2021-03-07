2ac6662b4a609777fc55e3d483dbfaa55cde3fc5
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-06 22:54:13 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(paramTech, paramNome) {
+  let arrayTec = [];
+  arrayTec = paramTech;
+  arrayTec.sort();
+  let name = '';
+  name = paramNome;
+  let novoArray = [];               
+  let objetoSeparado = {};
+  for (let index = 0; index < arrayTec.length; index += 1) {
+    objetoSeparado = { tech: arrayTec[index], name: name}
+    novoArray.push(objetoSeparado);
+  }
+  if (novoArray.length === 0) {
+    return 'Vazio';
+  } return novoArray;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
