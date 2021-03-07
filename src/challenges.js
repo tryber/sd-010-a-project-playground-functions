9d4a96b3dda86ea80e5ba4ec20bbbd531ac9de58
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-06 22:00:49 -0300

    Novos

--- src/challenges.js
+++ src/challenges.js
@@ -40,7 +40,7 @@ function concatName(paramNomes) {
 // Desafio 5
 function footballPoints(wins, ties) {
   let vit = wins * 3;
-  let emp = ties * 1;
+  let emp = ties;
   return vit + emp;
 }
 
@@ -65,21 +65,77 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = cat1 - mouse;
+  let dist2 = cat2 - mouse;
+  if (dist1 < 0) {
+    dist1 *= -1;
+  } if (dist2 < 0) {
+    dist2 *= -1;
+  }
+  if (dist1 > dist2) {
+    return 'cat2';
+  } if (dist1 === dist2) {
+    return 'os gatos trombam e o rato foge';
+  } return 'cat1';
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  function fizzBuzz(fizzBuzzParam) {
+    return fizzBuzzParam.map((numero) => {
+      if ((numero % 15) === 0) {
+        return 'fizzBuzz';
+      }
+      if ((numero % 3) === 0) {
+        return 'fizz';
+      }
+      if ((numero % 5) === 0) {
+        return 'buzz';
+      }
+      return 'bug!';
+    });
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(frase) {
+  let encodingPhrase = frase;
+  let newPhrase = '';
+  for (let index in encodingPhrase) {
+    if (encodingPhrase[index] === 'a') {
+      newPhrase += '1';
+    } else if (encodingPhrase[index] ==='e') {
+      newPhrase += '2';
+    } else if (encodingPhrase[index] ==='i') {
+      newPhrase += '3';
+    } else if (encodingPhrase[index] ==='o') {
+      newPhrase += '4';
+    } else if (encodingPhrase[index] ==='u') {
+      newPhrase += '5';
+    } else {
+      newPhrase += encodingPhrase[index];
+    }
+  }  return newPhrase;
 }
-function decode() {
-  // seu código aqui
+
+function decode(frase) {
+  let encodingPhrase = frase;
+  let newPhrase = '';
+  for (let index in encodingPhrase) {
+    if (encodingPhrase[index] === '1') {
+      newPhrase += 'a';
+    } else if (encodingPhrase[index] ==='2') {
+      newPhrase += 'e';
+    } else if (encodingPhrase[index] ==='3') {
+      newPhrase += 'i';
+    } else if (encodingPhrase[index] ==='4') {
+      newPhrase += 'o';
+    } else if (encodingPhrase[index] ==='5') {
+      newPhrase += 'u';
+    } else {
+      newPhrase += encodingPhrase[index];
+    }
+  }  return newPhrase;
 }
 
 module.exports = {
